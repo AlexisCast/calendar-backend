d9df32a81c9e4d5b576ece035a5471d3a401e226
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-09-15 21:22:20 -0600

    feat: 5 custom middleware fieldValidators

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 const router = Router();
 
+const { fieldValidators } = require("../middlewares/field-validators");
+
 const {
 	createUser,
 	loginUser,
@@ -20,6 +22,7 @@ router.post(
 		check("password", "The password must be 6 letters.").isLength({
 			min: 6,
 		}),
+		fieldValidators,
 	],
 	createUser
 );
@@ -31,6 +34,7 @@ router.post(
 		check("password", "The password must be 6 letters.").isLength({
 			min: 6,
 		}),
+		fieldValidators,
 	],
 	loginUser
 );
