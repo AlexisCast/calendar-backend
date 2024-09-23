55ed55f0af1e9caa759ca9bf5b6cf96cc99e3144
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-09-23 16:41:48 -0600

    feat: 18 update event to db

--- controllers/events.js
+++ controllers/events.js
@@ -31,10 +31,46 @@ const createEvent = async (req, res = response) => {
 };
 
 const updateEvent = async (req, res = response) => {
-	res.json({
-		ok: true,
-		msg: "update event",
-	});
+	const eventId = req.params.id;
+	const uid = req.uid;
+
+	try {
+		const event = await Event.findById(eventId);
+
+		if (!event) {
+			return res.status(404).json({
+				ok: false,
+				msg: "Event does not exist by id.",
+			});
+		}
+
+		if (event.user.toString() != uid) {
+			return res.status(401).json({
+				ok: false,
+				msg: "Not authorized to edit this event",
+			});
+		}
+
+		const newEvent = {
+			...req.body,
+			user: uid,
+		};
+
+		const updatedEvent = await Event.findByIdAndUpdate(eventId, newEvent, {
+			new: true,
+		});
+
+		res.json({
+			ok: true,
+			event: updatedEvent,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: "Contact the administrator.",
+		});
+	}
 };
 
 const deleteEvent = async (req, res = response) => {
