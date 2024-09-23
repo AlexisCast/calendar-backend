82bf2fd148fe21a27d62bea91a69b0341c825cab
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-09-22 18:23:14 -0600

    feat: 16 create event to db

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Event = require("../models/Event");
 
 const getEvents = async (req, res = response) => {
 	res.json({
@@ -8,11 +9,23 @@ const getEvents = async (req, res = response) => {
 };
 
 const createEvent = async (req, res = response) => {
-	console.log(req.body);
-	res.json({
-		ok: true,
-		msg: "create event",
-	});
+	const event = new Event(req.body);
+
+	try {
+		event.user = req.uid;
+		const eventSaved = await event.save();
+
+		res.json({
+			ok: true,
+			event: eventSaved,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: "Contact the administrator.",
+		});
+	}
 };
 
 const updateEvent = async (req, res = response) => {
